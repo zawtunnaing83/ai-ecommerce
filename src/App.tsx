399eff29658a32c6007b5f02dfe70bfd43e13cae
Zaw Tun Naing <tunnaingzaw@comfortdelgrobus.com.sg>
2024-10-04 00:09:05 +1300

    Add About Store and Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-
-
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
 function App() {
-  
-  return (  
-      <div>      
-    <h1>hi</h1>
-    </div>
-  )
+  return <Container> 
+    <Routes>
+
+    <Route path="/" element={<Home />} />
+    <Route path="/store" element={<Store />} />
+    <Route path="/about" element={<About />} />
+    </Routes>
+    </Container>
 }
 
 export default App
